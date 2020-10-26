7e5047db4ead33d7880d7c1c1a1273ad358b2758
kjwebguru <kjwebguru.1999@gmail.com>
2020-10-27 00:03:59 +0530

    added user backend support

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,4 +1,5 @@
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const uuid = require("uuid/v4");
 
@@ -13,55 +14,88 @@ const DUMMY_USERS = [
   },
 ];
 
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  let users;
+
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, Could not find users",
+      500
+    );
+    return next(error);
+  }
+
+  if (!users || users.length === 0) {
+    return next(new HttpError("No user found", 422));
+  }
+
+  res.json({
+    users: users.map((user) => user.toObject({ getters: true })),
+  });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const error = validationResult(req);
 
   if (!error.isEmpty()) {
-    throw new HttpError(
-      "Please enter valid credentials to create account",
-      422
+    return next(
+      new HttpError("Please enter valid credentials to create account", 422)
     );
   }
   const { name, email, password } = req.body;
-  const hasUser = DUMMY_USERS.find((u) => {
-    return u.email === email;
-  });
+  let hasUser;
 
-  if (hasUser) {
-    throw new HttpError("User already exist please try to login", 401);
+  try {
+    hasUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("No results found", 500);
+    return next(error);
   }
 
-  const createUser = {
-    id: uuid(),
+  if (hasUser) {
+    return next(new HttpError("User already exist please try to login", 401));
+  }
+
+  const createUser = new User({
     name,
     email,
     password,
-  };
+    image: "https://ibb.co/7RgZtm8",
+    places: "empire state building",
+  });
 
-  DUMMY_USERS.push(createUser);
+  try {
+    await createUser.save();
+  } catch (err) {
+    return next(
+      new HttpError("Something went wrong, Could not create user", 500)
+    );
+  }
 
-  res.status(201).json({ createdUser: createUser });
+  res.status(201).json({ createdUser: createUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const error = validationResult(req);
 
   if (!error.isEmpty()) {
-    throw new HttpError("Please enter valid email and password", 422);
+    return next(new HttpError("Please enter valid email and password", 422));
   }
 
   const { email, password } = req.body;
 
-  const loginUser = DUMMY_USERS.find((u) => {
-    return u.email === email && u.password === password;
-  });
+  let user;
+  try {
+    user = await User.findOne({ email: email, password: password });
+  } catch (err) {
+    const error = new HttpError("No user found", 500);
+    return next(error);
+  }
 
-  if (!loginUser) {
-    throw new HttpError("Couldn't find user try again", 422);
+  if (!user) {
+    return next(HttpError("Couldn't find user try again", 422));
   }
 
   res.json({ message: "You are logged in" });
--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
   places: { type: String, required: true },
 });
 
